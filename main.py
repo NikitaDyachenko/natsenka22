1aa6320ffadc8e708b46d54bf32ea31009ac53e4
NikitaDyachenko <125453885+NikitaDyachenko@users.noreply.github.com>
2023-11-12 18:00:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-
-class JSONData(BaseModel):
-    GrossProfit: float
-    RealizationProduct: float
 
 app = FastAPI()
-
-@app.get("/GrossProfitability/")
-async def products_man(item: JSONData):
-    GrossProfitability = str(round((item.GrossProfit/item.RealizationProduct)*100,2)) + " %"
-    info = {"GrossProfitability": GrossProfitability}
-    return JSONResponse(content=jsonable_encoder(info))
-
 @app.get("/ExtraCharge")
 async def get_extracharge(CostPrice, Percent):
     extracharge = (float(CostPrice))/(100-float(Percent)) * 100;
