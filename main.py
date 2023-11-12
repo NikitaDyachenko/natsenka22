83ce121d1a28d856fd64afa83d79d07d7f2d452c
NikitaDyachenko <125453885+NikitaDyachenko@users.noreply.github.com>
2023-11-12 17:45:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,6 @@ async def products_man(item: JSONData):
 
 @app.get("/ExtraCharge")
 async def get_extracharge(CostPrice, Percent):
-    extracharge = (float(CostPrice)/(100-float(Percent)) * 100;
+    extracharge = (float(CostPrice))/(100-float(Percent)) * 100;
     return {"Итоговая цена товара = ": str(extracharge)}
 
